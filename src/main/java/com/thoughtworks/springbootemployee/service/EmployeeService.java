9455499e257c26ed3345d365efaef277d298bc12
wyuefzhuol <1527169088@qq.com>
2020-07-28 17:01:01 +0800

    [SpringBoot][Eric] feature: create EmployeeService.addEmployee method

--- src/main/java/com/thoughtworks/springbootemployee/service/EmployeeService.java
+++ src/main/java/com/thoughtworks/springbootemployee/service/EmployeeService.java
@@ -1,5 +1,17 @@
 package com.thoughtworks.springbootemployee.service;
 
+import com.thoughtworks.springbootemployee.entity.Employee;
+import com.thoughtworks.springbootemployee.repository.EmployeeData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmployeeService {
 
+    @Autowired
+    EmployeeData employeeData;
+
+    public void addEmployee(Employee employee) {
+        employeeData.addEmployee(employee);
+    }
 }
