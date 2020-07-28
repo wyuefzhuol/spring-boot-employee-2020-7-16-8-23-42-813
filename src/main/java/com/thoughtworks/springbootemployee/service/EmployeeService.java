77831140cebe522e68fb186a14c33e9b1d438e53
wyuefzhuol <1527169088@qq.com>
2020-07-28 19:30:49 +0800

    [SpringBoot][Eric] feature: create method EmployeeService.getEmployees

--- src/main/java/com/thoughtworks/springbootemployee/service/EmployeeService.java
+++ src/main/java/com/thoughtworks/springbootemployee/service/EmployeeService.java
@@ -5,6 +5,8 @@ import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class EmployeeService {
 
@@ -13,5 +15,9 @@ public class EmployeeService {
 
     public void addEmployee(Employee employee) {
         employeeRepository.addEmployee(employee);
+    }
+
+    public List<Employee> getEmployees() {
+        return employeeRepository.getEmployees();
     }
 }
